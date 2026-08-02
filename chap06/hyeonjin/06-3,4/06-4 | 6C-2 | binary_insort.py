ae9c5204e1722ba78f11692bdcd13917401c86f6
hjin234 <shj48211558@gmail.com>
2026-08-02 15:54:17 +0900

    chap06/hyeonjin: 06-5

---
+++ chap06/hyeonjin/06-3,4/06-4 | 6C-2 | binary_insort.py
@@ -0,0 +1,20 @@
+from typing import MutableSequence
+import bisect
+
+def binary_insertion_sort(a: MutableSequence) -> None:
+    for i in range(1, len(a)):
+        bisect.insort(a, a.pop(i), 0, i)
+
+if __name__ == '__main__':
+    print('이진 삽입 정렬을 수행합니다.')
+    num = int(input('원소 수를 입력하세요.: '))
+    x = [None] * num
+
+    for i in range(num):
+        x[i] = int(input(f"x[{i}]: "))
+
+    binary_insertion_sort(x)
+
+    print('오름차순으로 정렬했습니다.')
+    for i in range(num):
+        print(f"x[{i}] = {x[i]}")
