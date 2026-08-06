32b421153c82af4b92e146f6f356a569040b6083
rhslxkd <ide1125@cau.ac.kr>
2026-08-06 16:49:49 +0900

    hyeonsoo 06-06

---
+++ chap06/hyeonsoo/06-05/sorted_sort.py
@@ -0,0 +1,18 @@
+print('sorted() 함수를 사용하여 정렬합니다.')
+num = int(input('원소 수를 입력하세요. : '))
+x = [None] * num
+
+for i in range(num):
+    x[i] = int(input(f'x[{i}]: '))
+
+# 배열 x를 오름차순으로 정렬
+x = sorted(x)
+print('오름차순으로 정렬했습니다')
+for i in range(num):
+    print(f'x[{i}] = {x[i]}')
+
+# 배열 x를 내림차순으로 정렬
+x = sorted(x, reverse = True)
+print('내림차순으로 정렬했습니다.')
+for i in range(num):
+    print(f'x[{i}] = {x[i]}')
